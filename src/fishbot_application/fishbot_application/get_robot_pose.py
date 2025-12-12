ae56f141b2dd0321b4ad1906c9e6ba03017fec78
haibo <zhaohaibo177417@gmail.com>
2025-12-12 00:13:06 -0600

    Initial commit

---
+++ src/fishbot_application/fishbot_application/get_robot_pose.py
@@ -0,0 +1,41 @@
+import rclpy
+from rclpy.node import Node
+import rclpy.time
+from tf2_ros import TransformListener,Buffer
+from tf_transformations import euler_from_quaternion
+import math
+
+
+
+class TFBroadcaster(Node):
+    def __init__(self):
+        super().__init__('tf_broadcaster')
+        self.buffer_ = Buffer()
+        self.listener_ = TransformListener(self.buffer_, self)
+        self.timer_ = self.create_timer(1.0, self.get_transform)
+        
+    def get_transform(self):
+        
+        try:
+            result = self.buffer_.lookup_transform('map', 'base_footprint',
+                    rclpy.time.Time(seconds=0.0), rclpy.time.Duration(seconds=1.0))
+            transform = result.transform
+            self.get_logger().info(f'平移: {transform.translation}')
+            self.get_logger().info(f'旋转: {transform.rotation}')
+            rotation_euler = euler_from_quaternion([
+                        transform.rotation.x,
+                        transform.rotation.y,
+                        transform.rotation.z,
+                        transform.rotation.w
+                        ])
+            self.get_logger().info(f'旋转RPY: {rotation_euler}')
+        except Exception as e:
+            self.get_logger().warn(f'获取变化失败:原因{str(e)}')
+            
+            
+            
+def main():
+    rclpy.init()
+    node = TFBroadcaster()
+    rclpy.spin(node)
+    rclpy.shutdown()
---
+++ src/fishbot_description/launch/gazebo_sim.launch.py
@@ -0,0 +1,107 @@
+import launch
+import launch.event_handlers
+import launch.launch_description_sources
+import launch_ros
+from ament_index_python.packages import get_package_share_directory
+import os
+
+import launch_ros.parameter_descriptions
+
+
+
+def generate_launch_description():
+    
+    urdf_package_path = get_package_share_directory('fishbot_description')
+    default_xacro_path = os.path.join(urdf_package_path, 'urdf', 'fishbot/fishbot.urdf.xacro')
+    # default_rviz_config_path = os.path.join(urdf_package_path, 'config', 'display_robot_model.rviz')
+    default_gazebo_world_path = os.path.join(urdf_package_path, 'world', 'custom_room.world')
+    
+    # 添加参数名为model:= xxxxxxxxx
+    action_declare_arg_mode_path = launch.actions.DeclareLaunchArgument(
+        name='model',default_value=str(default_xacro_path),
+        description='加载模型文件.'
+    )
+    
+    substitutions_command_result = launch.substitutions.Command(['xacro ', launch.substitutions.LaunchConfiguration('model')])
+    
+    robot_description_value = launch_ros.parameter_descriptions.ParameterValue(substitutions_command_result, value_type=str)
+    
+    action_robot_state_publisher = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description':robot_description_value}]
+    )
+    
+    action_launch_gazebo = launch.actions.IncludeLaunchDescription(
+        launch.launch_description_sources.PythonLaunchDescriptionSource(
+            [get_package_share_directory('gazebo_ros'), '/launch', '/gazebo.launch.py']
+        ),
+        # launch_arguments=[{'world', default_gazebo_world_path},{'verbose','true'}]
+        launch_arguments={'world': default_gazebo_world_path,
+                  'verbose': 'true'}.items()
+    )
+    
+    action_spawn_entity = launch_ros.actions.Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', '/robot_description', '-entity', 'fishbot']
+    )
+    
+    action_load_joint_state_controller = launch.actions.ExecuteProcess(
+        cmd='ros2 control load_controller fishbot_joint_state_broadcaster --set-state active'.split(' '),
+        output='screen'
+    )
+    
+    # 力控制
+    action_load_effort_controller = launch.actions.ExecuteProcess(
+        cmd='ros2 control load_controller fishbot_effort_controller --set-state active'.split(' '),
+        output='screen'
+    )
+    
+    # 两轮差速控制
+    action_load_diff_driver_controller = launch.actions.ExecuteProcess(
+        cmd='ros2 control load_controller fishbot_diff_drive_controller --set-state active'.split(' '),
+        output='screen'
+    )
+    
+    
+    # package='joint_state_publisher_gui',
+    # executable='joint_state_publisher_gui'
+    # action_joint_state_publisher = launch_ros.actions.Node(
+    #     package='joint_state_publisher_gui',
+    #     executable='joint_state_publisher_gui'
+    # )
+    
+    # action_rviz_node = launch_ros.actions.Node(
+    #     package='rviz2',
+    #     executable='rviz2',
+    #     arguments=['-d', default_rviz_config_path]
+    # )
+    
+    
+    return launch.LaunchDescription([
+        action_declare_arg_mode_path,
+        action_robot_state_publisher,
+        # action_joint_state_publisher,
+        action_spawn_entity,
+        action_launch_gazebo,
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=action_spawn_entity,
+                on_exit=[action_load_joint_state_controller],
+            )
+        ),
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=action_spawn_entity,
+                on_exit=[action_load_effort_controller],
+            )
+        ),
+        # launch.actions.RegisterEventHandler(
+        #     event_handler=launch.event_handlers.OnProcessExit(
+        #         target_action=action_spawn_entity,
+        #         on_exit=[action_load_diff_driver_controller],
+        #     )
+        # ),
+        # action_rviz_node,
+    ])
